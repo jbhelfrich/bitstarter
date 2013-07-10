29136182d34659aaf2fdef8a220f2393a4e00983
Joe Helfrich <joe@helfrich.us>
2013-07-10 14:32:23 -0400

    Using index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var resultBuffer;
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  result = fs.readFileSync('index.html', 'utf-8')
+  response.send(result);
 });
 
 var port = process.env.PORT || 5000;
